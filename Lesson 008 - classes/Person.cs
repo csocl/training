35ec58d71d1bf2f8b83d104463f353c9e73c8c71
colinrg <colingirling@googlemail.com>
2018-02-27 21:09:44 +0000

    Updated the comments for lesson 008 - classes.

--- Lesson 008 - classes/Person.cs
+++ Lesson 008 - classes/Person.cs
@@ -15,8 +15,29 @@ namespace Lesson_008___classes
      */
     public class Person
     {
+        /*
+         * When defining a method for a class, you can specify the method name,
+         * the return type which is left of the method name, and any parameters
+         * which are within the brackets following the method name.
+         * Methods also known as functions, can access data within the class,
+         * such as Name or Age.
+         * 
+         * There are keywords like public and private that allow us to define
+         * what the user of our class can see.
+         * 
+         * When something is marked as private then only the class can see those
+         * functions or variables.
+         * 
+         * When something is marked as public, the class can see those functions
+         * and variables, but so can the user of the class.
+         */
         public string GetName()
         {
+            /*
+             * To return a value to the user of our class object,
+             * we use the keyword return followed by the name of the variable
+             * that will be returned.
+             */
             return Name;
         }
 
@@ -35,6 +56,11 @@ namespace Lesson_008___classes
             Age = age;
         }
 
+        /*
+         * When defining a variable within a class,
+         * you just need to provide the type and the name of the variable.
+         * We can see the type for variable Name is string.
+         */
         private string Name;
         private uint Age;
     }
--- Lesson 008 - classes/Program.cs
+++ Lesson 008 - classes/Program.cs
@@ -18,9 +18,14 @@ namespace Lesson_008___classes
             person.SetName("Fred");
             person.SetAge(22);
 
+            // We can extract the values from the person object by calling the functions
+            // that return the values.
+            string Name = person.GetName();
+            uint Age = person.GetAge();
+
             // To prove this is set as expected, we can get the values out of the person
             // object and display them to the screen.
-            Console.WriteLine(person.GetName() + " is " + person.GetAge() + " years old");
+            Console.WriteLine(Name + " is " + Age + " years old");
         }
     }
 }
