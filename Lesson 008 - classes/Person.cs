24ddb2f35bab3874fa86eb00d069fb6794f5e053
colinrg <colingirling@googlemail.com>
2018-02-27 20:11:23 +0000

    Added Lesson 008 - classes for issue #8

---
+++ Lesson 008 - classes/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_008___classes
+{
+    /*
+     * Classes allow methods and data to be brought together into a logical unit.
+     * This is known as encapsulation, and allows us to safeguard how the data is
+     * modified through the methods of the class.
+     * By hiding the data and only accessing them through the methods,
+     * we can control the state of our data, and ensure they remain coherent together.
+     */
+    public class Person
+    {
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public uint GetAge()
+        {
+            return Age;
+        }
+
+        public void SetAge(uint age)
+        {
+            Age = age;
+        }
+
+        private string Name;
+        private uint Age;
+    }
+}
---
+++ Lesson 008 - classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_008___classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Any class is a reference type, and must be constructed using the new keyword.
+            Person person = new Person();
+
+            // Once the person object has been created, the methods on the person object
+            // can be used to set the state of the data for the person object.
+            person.SetName("Fred");
+            person.SetAge(22);
+
+            // To prove this is set as expected, we can get the values out of the person
+            // object and display them to the screen.
+            Console.WriteLine(person.GetName() + " is " + person.GetAge() + " years old");
+        }
+    }
+}
