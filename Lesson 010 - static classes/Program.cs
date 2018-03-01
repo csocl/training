e0727064ca27858c97f87ca8a40526a726850ba7
colinrg <colingirling@googlemail.com>
2018-03-01 22:01:48 +0000

    Added Lesson 010 - static classes for issue #10

---
+++ Lesson 010 - static classes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_010___static_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string found, notFound;
+            string str = "ABC123, def456.";
+
+            Console.WriteLine("Separate [" + str + "]");
+
+            /*
+             * As this is a static class, there is no need to have a variable for StringUtility.
+             * Methods are called directly on the static class.
+             */
+            StringUtility.Separate(str, out found, out notFound, StringUtility.SeparateOptions.Digits);
+            int count = StringUtility.CountChars(str, StringUtility.SeparateOptions.Digits);
+            Console.WriteLine("digits [" + found + "] other [" + notFound + "] count = " + count);
+
+            StringUtility.Separate(str, out found, out notFound, StringUtility.SeparateOptions.Letters);
+            count = StringUtility.CountChars(str, StringUtility.SeparateOptions.Letters);
+            Console.WriteLine("letters [" + found + "] other [" + notFound + "] count = " + count);
+
+            StringUtility.Separate(str, out found, out notFound, StringUtility.SeparateOptions.Punctuation);
+            count = StringUtility.CountChars(str, StringUtility.SeparateOptions.Punctuation);
+            Console.WriteLine("punctuation [" + found + "] other [" + notFound + "] count = " + count);
+
+            StringUtility.Separate(str, out found, out notFound, StringUtility.SeparateOptions.WhiteSpace);
+            count = StringUtility.CountChars(str, StringUtility.SeparateOptions.WhiteSpace);
+            Console.WriteLine("whitespace [" + found + "], other [" + notFound + "] count = " + count);
+        }
+    }
+}
---
+++ Lesson 010 - static classes/StringUtility.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_010___static_classes
+{
+    /*
+     * Classes declared as static can be used without having
+     * a variable declared for the class type.
+     * These classes don't take up memory, and the methods can
+     * be used by just specifying the class name, full stop and method name.
+     * e.g.
+     * *   int count = StringUtility.CountChars("2a .");
+     *     // count will be 1.
+     */
+    static public class StringUtility
+    {
+        public enum SeparateOptions
+        {
+            Digits,
+            Letters,
+            WhiteSpace,
+            Punctuation
+        };
+
+        // Count the number of characters matching the option.
+        public static int CountChars(string str, SeparateOptions option)
+        {
+            string found, notFound;
+            Separate(str, out found, out notFound, option);
+            return found.Length;
+        }
+
+        // Separate characters from a string into two strings.
+        public static void Separate(string str, out string found, out string notFound, SeparateOptions option)
+        {
+            found = "";
+            notFound = "";
+
+            foreach(char ch in str)
+            {
+                switch (option)
+                {
+                    case SeparateOptions.Digits:
+                        if (char.IsDigit(ch))
+                            found += ch;
+                        else
+                            notFound += ch;
+                        break;
+                    case SeparateOptions.Letters:
+                        if (char.IsLower(ch) || char.IsUpper(ch))
+                            found += ch;
+                        else
+                            notFound += ch;
+                        break;
+                    case SeparateOptions.Punctuation:
+                        if (char.IsPunctuation(ch))
+                            found += ch;
+                        else
+                            notFound += ch;
+                        break;
+                    case SeparateOptions.WhiteSpace:
+                        if (char.IsWhiteSpace(ch))
+                            found += ch;
+                        else
+                            notFound += ch;
+                        break;
+                }
+            }
+        }
+    }
+}
